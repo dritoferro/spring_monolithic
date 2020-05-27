4ece019580e23575a49593b13463407fddc83900
Adriano <adrianotagliaferro@hotmail.com>
2020-05-27 11:53:40 -0300

    Created base repositories for domain entities.

---
+++ src/main/kotlin/br/com/tagliaferrodev/samplerest/repositories/PessoaRepository.kt
@@ -0,0 +1,8 @@
+package br.com.tagliaferrodev.samplerest.repositories
+
+import br.com.tagliaferrodev.samplerest.domain.Pessoa
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PessoaRepository : JpaRepository<Pessoa, Long>
--- src/main/kotlin/br/com/tagliaferrodev/samplerest/repositories/UsuarioRepository.kt
+++ src/main/kotlin/br/com/tagliaferrodev/samplerest/repositories/UsuarioRepository.kt
@@ -3,6 +3,10 @@ package br.com.tagliaferrodev.samplerest.repositories
 import br.com.tagliaferrodev.samplerest.domain.Usuario
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface UsuarioRepository : JpaRepository<Usuario, Long>
+interface UsuarioRepository : JpaRepository<Usuario, Long> {
+
+    fun findByEmailOrApelido(email: String, apelido: String): Optional<Usuario>
+}
