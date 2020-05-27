017115d9dd4be70c51dc006480bed70be09856c5
Adriano <adrianotagliaferro@hotmail.com>
2020-05-27 20:19:00 -0300

    Moved base route path for application.properties

--- src/main/kotlin/br/com/tagliaferrodev/samplerest/controllers/PaisController.kt
+++ src/main/kotlin/br/com/tagliaferrodev/samplerest/controllers/PaisController.kt
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("v1/paises")
+@RequestMapping("paises")
 class PaisController(val service: PaisService) {
 
     @PostMapping
