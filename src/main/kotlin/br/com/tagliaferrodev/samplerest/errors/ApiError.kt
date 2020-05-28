fc3a03cff32374475b63b97b00dda189127e24f1
Adriano <adrianotagliaferro@hotmail.com>
2020-05-28 17:07:36 -0300

    Updated handlers for errors.

--- src/main/kotlin/br/com/tagliaferrodev/samplerest/errors/ApiError.kt
+++ src/main/kotlin/br/com/tagliaferrodev/samplerest/errors/ApiError.kt
@@ -2,14 +2,13 @@ package br.com.tagliaferrodev.samplerest.errors
 
 import org.springframework.http.HttpStatus
 import java.time.Instant
-import java.util.*
 
 data class ApiError(
         val status: HttpStatus,
         var message: String?,
         val timestamp: Long = Instant.now().toEpochMilli(),
         val debugMessage: String? = null,
-        val subErrors: ArrayList<ApiSubError>? = null) {
+        val subErrors: List<ApiSubError> = listOf()) {
     init {
         message = message ?: "Something went wrong."
     }
--- src/main/kotlin/br/com/tagliaferrodev/samplerest/errors/ApiExceptionHandler.kt
+++ src/main/kotlin/br/com/tagliaferrodev/samplerest/errors/ApiExceptionHandler.kt
@@ -1,5 +1,6 @@
 package br.com.tagliaferrodev.samplerest.errors
 
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -11,19 +12,38 @@ import javax.validation.ConstraintViolationException
 @RestControllerAdvice
 class ApiExceptionHandler : ResponseEntityExceptionHandler() {
 
+    private fun sendResponse(apiError: ApiError): ResponseEntity<ApiError> {
+        return ResponseEntity(apiError, apiError.status)
+    }
+
     @ExceptionHandler(EntityNotFoundException::class)
-    fun handleNotFound(ex: EntityNotFoundException): ResponseEntity<Any> {
+    fun handleNotFound(ex: EntityNotFoundException): ResponseEntity<ApiError> {
         return ResponseEntity(HttpStatus.NOT_FOUND)
     }
 
     @ExceptionHandler(IllegalArgumentException::class)
-    fun handleIllegalArgument(ex: IllegalArgumentException): ResponseEntity<Any> {
+    fun handleIllegalArgument(ex: IllegalArgumentException): ResponseEntity<ApiError> {
         return ResponseEntity(HttpStatus.BAD_REQUEST)
     }
 
     @ExceptionHandler(ConstraintViolationException::class)
-    fun handleConstraintViolation(ex: ConstraintViolationException): ResponseEntity<Any> {
-        return ResponseEntity(ex.constraintViolations.map { "${it.propertyPath}: ${it.message}" }, HttpStatus.BAD_REQUEST)
+    fun handleConstraintViolation(ex: ConstraintViolationException): ResponseEntity<ApiError> {
+        val subErrors = ex.constraintViolations.map {
+            ApiSubError(
+                    field = it.propertyPath.toString(),
+                    rejectedValue = it.invalidValue.toString(),
+                    message = it.message
+            )
+        }
+        val error = ApiError(status = HttpStatus.BAD_REQUEST, message = "Ocorreu um erro na validação dos dados", subErrors = subErrors)
+
+        return sendResponse(error)
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException::class)
+    fun handleDataIntegrityViolation(ex: DataIntegrityViolationException): ResponseEntity<ApiError> {
+        val error = ApiError(status = HttpStatus.BAD_REQUEST, message = "Houve algum erro na camada de dados", debugMessage = ex.message)
+        return sendResponse(error)
     }
 
     @ExceptionHandler(Exception::class)
