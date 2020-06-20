64ca0383023e916283d0f569b48f95c00620dd5e
Adriano <adrianotagliaferro@hotmail.com>
2020-06-20 11:25:01 -0300

    Added missing test for EstadoService.

--- src/test/kotlin/br/com/tagliaferrodev/samplerest/services/EstadoServiceTest.kt
+++ src/test/kotlin/br/com/tagliaferrodev/samplerest/services/EstadoServiceTest.kt
@@ -3,10 +3,16 @@ package br.com.tagliaferrodev.samplerest.services
 import br.com.tagliaferrodev.samplerest.domain.Estado
 import br.com.tagliaferrodev.samplerest.domain.Pais
 import br.com.tagliaferrodev.samplerest.repositories.EstadoRepository
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
+import java.util.*
+import javax.persistence.EntityNotFoundException
 
 @ExtendWith(MockitoExtension::class)
 class EstadoServiceTest : CRUDTestExecutor<Estado, EstadoService, EstadoRepository>() {
@@ -47,4 +53,21 @@ class EstadoServiceTest : CRUDTestExecutor<Estado, EstadoService, EstadoReposito
     )
 
     override var mainEntityWithId = mainEntity.copy(id = mainEntityId)
+
+    @Test
+    fun findByPaisShouldReturnSuccessfully() {
+        `when`(repository.findAllByPais_Id(1)).thenReturn(Optional.of(entities))
+
+        val search = service.findByPaisId(1)
+
+        assertEquals(5, search.estados?.size)
+        assertEquals(mainEntity.nome, search.estados?.get(0)?.nome)
+    }
+
+    @Test
+    fun findByPaisShouldThrowException() {
+        assertThrows<EntityNotFoundException> {
+            service.findByPaisId(1)
+        }
+    }
 }
